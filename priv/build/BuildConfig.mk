d529ae21156ce3f5ee84ad38835d00fb4b45ee60
Davide Marquês <davide@dav-mobile.(none)>
2009-11-13 00:53:49 +0000

    - Removed hardcoded paths to erl_interface dir. - binext="" for gcc since it won't be used on windows.

--- priv/build/BuildConfig.mk
+++ priv/build/BuildConfig.mk
@@ -1,11 +1,13 @@
+ERL=$(shell which erl)
+ERLC=$(shell which erlc)
+
+ERL_LIB_DIR=$(shell dirname $(ERL))/../lib
+ERL_INTERFACE=$(shell ls $(ERL_LIB_DIR) | grep erl_interface | tail -1)
+ERL_INTERFACE_DIR=$(ERL_LIB_DIR)/$(ERL_INTERFACE)
 
 BIN_EXT=.exe
 #BIN_EXT=
 
-ERL=erl
-ERLC= erlc
 PROJECTS_DIR=../../c_src
 
-ERL_INTERFACE_DIR=c:/erl5.7.2/lib/erl_interface-3.6.2
-#ERL_INTERFACE_DIR=/usr/local/lib/erlang/lib/erl_interface-3.6.2
 
